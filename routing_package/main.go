b3f7048ae4d1c084d0a64b55121908fe990f1dd9
Fernando Sanchez <fisanchez@github.com>
2023-05-04 23:09:00 -0500

    Create placeholders for REST verbs

--- routing_package/main.go
+++ routing_package/main.go
@@ -2,17 +2,53 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
+
+type Task struct {
+	Id   int       `json:"id"`
+	Text string    `json:"text"`
+	Tags []string  `json:"tags"`
+	Due  time.Time `json:"due"`
+}
+
+var task []Task
 
 func main() {
 	router := chi.NewRouter()
+
+	// Logs server requests to terminal
+	router.Use(middleware.Logger)
 
 	// router
 	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Write([]byte("Hello World!"))
 	})
 
+	// Get(pattern string, h http.HandlerFunc)
+	router.Get("/tasks", getTasks)
+	router.Get("/tasks/{id}", getTask)
+	router.Post("/tasks", postTask)
+	router.Post("/tasks/{id}", deleteTask)
+
 	http.ListenAndServe(":8080", router)
+}
+
+func getTasks(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Getting all tasks!"))
+}
+
+func getTask(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Getting a task!"))
+}
+
+func postTask(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Posting a task!"))
+}
+
+func deleteTask(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Deleting a task!"))
 }
